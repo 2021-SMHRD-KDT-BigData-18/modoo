dafc96d5261b581cdc7b9a4e45ead3394dc040ac
smhrd <smhrd@221.156.60.75>
2023-05-15 10:16:09 +0900

    whdgus

--- MODOO/src/main/java/modoo/controller/GoModooCon.java
+++ MODOO/src/main/java/modoo/controller/GoModooCon.java
@@ -6,12 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class GoModoo implements Controller {
+public class GoModooCon implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
