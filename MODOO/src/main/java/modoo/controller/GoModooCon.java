537d5e2b0aa90625e1dd5eb92cc6f8c31e28eb7f
smhrd <smhrd@221.156.60.75>
2023-05-19 12:21:16 +0900

    종현 jsp

--- MODOO/src/main/java/modoo/controller/GoModooCon.java
+++ MODOO/src/main/java/modoo/controller/GoModooCon.java
@@ -6,13 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class GoCultureCon implements Controller {
+public class GoModooCon implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		return "culture";
+		return "modoo";
 	}
 
 	
