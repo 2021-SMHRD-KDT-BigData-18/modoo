45b8088affb3e89ea1b0c3f48ce4ff27ef4190a4
smhrd <smhrd@221.156.60.75>
2023-05-23 13:53:54 +0900

    dd

---
+++ MODOO/src/main/webapp/js/cul_2.js
@@ -0,0 +1,80 @@
+$(function() {
+	$("#cul_h2_btn1").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn1.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open1").html(data);
+				}
+		});
+	})
+})
+$(function() {
+	$("#cul_h2_btn2").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn2.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open2").html(data);
+				}
+		});
+	})
+})
+$(function() {
+	$("#cul_h2_btn3").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn3.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open3").html(data);
+				}
+		});
+	})
+})
+$(function() {
+	$("#cul_h2_btn4").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn4.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open4").html(data);
+				}
+		});
+	})
+})
+$(function() {
+	$("#cul_h2_btn5").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn5.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open5").html(data);
+				}
+		});
+	})
+})
+$(function() {
+	$("#cul_h2_btn6").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn6.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open6").html(data);
+				}
+		});
+	})
+})
+$(function() {
+	$("#cul_h2_btn7").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn7.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open7").html(data);
+				}
+		});
+	})
+})
+$(function() {
+	$("#cul_h2_btn8").click(function() {
+		$.ajax({
+			type: 'post', url:
+				'/htmlfile3/cul_h2_btn8.jsp', dataType: 'html', success: function(data) {
+					$("#btn_open8").html(data);
+				}
+		});
+	})
+})
